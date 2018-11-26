17e8a5d7788000189f83457fc41b177b2d94380a
Ben Raymond <ben.raymond@aad.gov.au>
2018-11-26 15:35:35 +1100

    minor code refactor

--- R/datasets.R
+++ R/datasets.R
@@ -13,18 +13,9 @@
 #'
 #' @export
 qa_datasets <- function(cache_directory, refresh_cache = FALSE, verbose = FALSE) {
-    cache_directory <- deal_with_cache_dir(cache_directory, verbose = verbose)
-    mirror_dir <- sub("/+$", "", sub("^(http|https|ftp)://", "", qa_mirror())) ## remove protocol prefix and trailing sep
-    index_file <- file.path(cache_directory, mirror_dir, "Quantarctica3/Quantarctica3.qgs")
-    if (!file.exists(index_file) || refresh_cache) {
-        if (!dir.exists(dirname(index_file))) tryCatch(dir.create(dirname(index_file), recursive = TRUE), error = function(e) stop("Could not create cache_directory: ", dirname(index_file)))
-        res <- bb_rget(url = paste0(qa_mirror(), "Quantarctica3/Quantarctica3.qgs"), force_local_filename = index_file, use_url_directory = FALSE, verbose = verbose)
-    }
-    if (file.exists(index_file)) {
-        lx <- xml2::read_xml(index_file)
-        get_layer_details <- function(z) as.data.frame(as.list(xml2::xml_attrs(z))[c("name", "source")], stringsAsFactors = FALSE)
-        lxs <- as_tibble(do.call(rbind, lapply(xml2::xml_find_all(lx, ".//layer-tree-layer"), get_layer_details)))
-        lxs$source <- file.path(cache_directory, gsub("^.*Quantarctica3/", "", lxs$source))
+    lxs <- dataset_index(cache_directory, refresh_cache = refresh_cache, verbose = verbose)
+    if (!is.null(lxs)) {
+        lxs$source <- file.path(cache_directory, lxs$source)
         lxs$cached <- vapply(lxs$source, file.exists, FUN.VALUE = TRUE, USE.NAMES = FALSE)
         lxs
     } else {
@@ -33,3 +24,27 @@ qa_datasets <- function(cache_directory, refresh_cache = FALSE, verbose = FALSE)
     }
 }
 
+## internal function to get dataset index
+dataset_index <- function(cache_directory, refresh_cache = FALSE, verbose = FALSE) {
+    cache_directory <- deal_with_cache_dir(cache_directory, verbose = verbose)
+    mirror_dir <- sub("/+$", "", sub("^(http|https|ftp)://", "", qa_mirror())) ## remove protocol prefix and trailing sep
+    index_file <- file.path(cache_directory, mirror_dir, "Quantarctica3/Quantarctica3.qgs")
+    if (!file.exists(index_file) || refresh_cache) {
+        if (!dir.exists(dirname(index_file))) tryCatch(dir.create(dirname(index_file), recursive = TRUE), error = function(e) stop("Could not create cache_directory: ", dirname(index_file)))
+        res <- bb_rget(url = paste0(qa_mirror(), "Quantarctica3/Quantarctica3.qgs"), force_local_filename = index_file, use_url_directory = FALSE, verbose = verbose)
+    }
+    if (file.exists(index_file)) {
+        dataset_qgs_to_tibble(index_file)
+    } else {
+        NULL
+    }
+}
+
+## internal function to turn Quantarctica3.qgs file into tibble
+dataset_qgs_to_tibble <- function(index_file) {
+        lx <- xml2::read_xml(index_file)
+        get_layer_details <- function(z) as.data.frame(as.list(xml2::xml_attrs(z))[c("name", "source")], stringsAsFactors = FALSE)
+        lxs <- as_tibble(do.call(rbind, lapply(xml2::xml_find_all(lx, ".//layer-tree-layer"), get_layer_details)))
+        lxs$source <- sub("^.*Quantarctica3/", "", lxs$source)
+        lxs
+}
