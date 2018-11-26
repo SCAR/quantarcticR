f596dce44e30f9545848287a2dd0d9429234ea0d
Ben Raymond <ben.raymond@aad.gov.au>
2018-11-26 16:02:00 +1100

    break qa_dataset into exported function

--- R/datasets.R
+++ R/datasets.R
@@ -1,7 +1,51 @@
+#' Retrieve details of a Quantarctica data set
+#'
+#' @param name string: the name of the data set
+#' @param cache_directory string: as for \code{qa_get}
+#' @param refresh_cache logical: as for \code{qa_get}
+#' @param verbose logical: show progress messages?
+#'
+#' @return A tibble
+#'
+#' @export
+qa_dataset <- function(name, cache_directory, refresh_cache = FALSE, verbose = FALSE) {
+    q_mirror <- file.path(sub("/$", "", qa_mirror()), "Quantarctica3")
+
+    ## find name in datasets index
+    lx <- dataset_index(cache_directory, refresh_cache = FALSE, verbose = FALSE, expand_source = FALSE)
+    idx <- lx$name == name
+    if (sum(idx) < 1) {
+        ## try case-insensitive
+        idx <- tolower(lx$name) == tolower(name)
+    }
+    if (sum(idx) < 1) {
+        stop("no matching data set found")
+    } else if (sum(idx) > 1) {
+        stop("multiple matching data sets found")
+    } else {
+        path <- dirname(lx$source[idx])
+    }
+    bb_source(
+        name = name,
+        id = paste0("Quantarctica: ", name),
+        description = "Quantarctica data",
+        doc_url = "http://quantarctica.npolar.no/",
+        citation = paste0("Matsuoka, K., Skoglund, A., & Roth, G. (2018). Quantarctica ", name, ". Norwegian Polar Institute. https://doi.org/10.21334/npolar.2018.8516e961"),
+        source_url = sub("//$", "/", paste0(file.path(q_mirror, path), "/")),
+        license = "CC-BY 4.0 International",
+        method = list("bb_handler_rget", level = 2, accept_download_extra = "(cpg|dbf|prj|qix|shp|shx)$"),
+        postprocess = NULL##list("bb_unzip")##,
+        ##collection_size = 0.6,
+        ##data_group = "Topography")
+    )
+}
+
+
 #' Available Quantarctica data sets
 #'
 #' @param cache_directory string: as for \code{qa_get}
 #' @param refresh_cache logical: as for \code{qa_get}
+#' @param verbose logical: show progress messages?
 #'
 #' @return A tibble with columns \code{id}, \code{name}, \code{source}, and \code{cached}
 #'
--- R/fetch_data.R
+++ R/fetch_data.R
@@ -1,41 +1,6 @@
-## internal helper function
-build_src <- function(name, cache_directory) {
-    q_mirror <- file.path(sub("/$", "", qa_mirror()), "Quantarctica3")
-
-    ## find name in datasets index
-    lx <- dataset_index(cache_directory, refresh_cache = FALSE, verbose = FALSE, expand_source = FALSE)
-    idx <- lx$name == name
-    if (sum(idx) < 1) {
-        ## try case-insensitive
-        idx <- tolower(lx$name) == tolower(name)
-    }
-    if (sum(idx) < 1) {
-        stop("no matching data set found")
-    } else if (sum(idx) > 1) {
-        stop("multiple matching data sets found")
-    } else {
-        path <- dirname(lx$source[idx])
-    }
-    bb_source(
-        name = name,
-        id = paste0("Quantarctica: ", name),
-        description = "Quantarctica data",
-        doc_url = "http://quantarctica.npolar.no/",
-        citation = "Matsuoka, K., Skoglund, A., & Roth, G. (2018). Quantarctica [Data set]. Norwegian Polar Institute. https://doi.org/10.21334/npolar.2018.8516e961",
-        source_url = sub("//$", "/", paste0(file.path(q_mirror, path), "/")),
-        license = "CC-BY 4.0 International",
-        method = list("bb_handler_rget", level = 2, accept_download_extra = "(cpg|dbf|prj|qix|shp|shx)$"),
-        postprocess = NULL##list("bb_unzip")##,
-        ##collection_size = 0.6,
-        ##data_group = "Topography")
-    )
-}
-
-
-
 #' Fetch a Quantarctica data set
 #'
-#' @param dataset string: the name of the data set
+#' @param dataset string or tibble: the name of the data set or a tibble as returned by \code{qa_dataset}
 #' @param cache_directory string: (optional) cache the data locally in this directory, so that they can be used offline later. Values can be "session" (a per-session temporary directory will be used, default), "persistent" (the directory returned by \code{rappdirs::user_cache_dir} will be used), or a string giving the path to the directory to use. Use \code{NULL} for no caching. An attempt will be made to create the cache directory if it does not exist. Use \code{refresh_cache = TRUE} to refresh the cached data if necessary
 #' @param refresh_cache logical: if TRUE, and data already exist in the cache_directory, they will be refreshed. If FALSE, the cached data will be used
 #' @param verbose logical: show progress messages?
@@ -47,8 +12,8 @@ qa_get <- function(dataset, cache_directory, refresh_cache = FALSE, verbose = FA
     assert_that(is.flag(refresh_cache), !is.na(refresh_cache))
     assert_that(is.flag(verbose), !is.na(verbose))
     cache_directory <- deal_with_cache_dir(cache_directory)
-    mysrc <- build_src(dataset, cache_directory)
-    bb_get(mysrc, local_file_root = cache_directory, clobber = as.integer(refresh_cache), verbose = verbose)
+    if (is.string(dataset)) dataset <- qa_dataset(dataset, cache_directory = cache_directory, refresh_cache = refresh_cache, verbose = verbose)
+    bb_get(dataset, local_file_root = cache_directory, clobber = as.integer(refresh_cache), verbose = verbose)
 }
 
 ## internal function to
