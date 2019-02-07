88fb017c7ba5ebdfd7dc01a8c6ab28522aae9a44
Ben Raymond <ben.raymond@aad.gov.au>
2019-02-07 18:06:06 +1100

    import utils::menu

--- R/quantarcticR-package.R
+++ R/quantarcticR-package.R
@@ -1,5 +1,6 @@
 #' @importFrom assertthat assert_that is.flag is.string
 #' @importFrom tibble as_tibble tribble
 #' @importFrom bowerbird bb_get bb_rget bb_source
+#' @importFrom utils menu
 #' @keywords internal
 "_PACKAGE"
