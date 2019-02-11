7a0b5b722367bc7f39cec6a740e2a02c12fd31d4
Ben Raymond <ben.raymond@aad.gov.au>
2019-02-12 09:52:34 +1100

    test custom reader in qa_get

--- tests/testthat/test_qa_get.R
+++ tests/testthat/test_qa_get.R
@@ -1,10 +1,15 @@
 
 context("Fetch Dataset")
 
-res <- qa_get("ADD Simple basemap", verbose = TRUE)
 
-test_that("The dataset from qa_get returns a shapefile or raster, as appropriate",{
+test_that("The dataset from qa_get returns a shapefile or raster, as appropriate", {
+    res <- qa_get("ADD Simple basemap", verbose = TRUE)
     expect_is(res, class = "SpatialPolygonsDataFrame")
     ## TODO add test on raster data
 })
 
+test_that("qa_get can be passed a custom reader", {
+    skip_if_not_installed("sf")
+    res <- qa_get("ADD Simple basemap", shapefile_reader = sf::st_read)
+    expect_is(res, class = "sf")
+})
