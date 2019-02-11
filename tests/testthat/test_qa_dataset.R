a6a142de6452acbe53e698ab97761bb9d94493fe
Ben Raymond <ben.raymond@aad.gov.au>
2019-02-12 08:53:56 +1100

    test for duplicate data layer names

--- tests/testthat/test_qa_dataset.R
+++ tests/testthat/test_qa_dataset.R
@@ -1,8 +1,14 @@
 
 context("Dataset details from qa_dataset")
 
-ds1 <- qa_dataset("ADD Simple basemap")
 
 test_that("The dataset requested from qa_dataset returns a custom class qa_dataset",{
-  expect_is(ds1, class = "qa_dataset")
+    ds1 <- qa_dataset("ADD Simple basemap")
+    expect_is(ds1, class = "qa_dataset")
+})
+
+test_that("Duplicate data layers have been removed from the dataset list", {
+    dss <- qa_datasets()
+    expect_false(any(duplicated(dss$layername)))
+    ds1 <- qa_dataset("ADD Coastlines (high)") ## will error if duplicates have not been removed
 })
