6f566884cafbb15214c8bf8a54fcee7b6cc13757
Ben Raymond <ben.raymond@aad.gov.au>
2019-02-13 10:30:03 +1100

    tests

--- tests/testthat/test_qa_get.R
+++ tests/testthat/test_qa_get.R
@@ -1,11 +1,15 @@
-
 context("Fetch Dataset")
 
-
 test_that("The dataset from qa_get returns a shapefile or raster, as appropriate", {
-    res <- qa_get("ADD Simple basemap", verbose = TRUE)
+    res <- qa_get("ADD Simple basemap")
     expect_is(res, class = "SpatialPolygonsDataFrame")
     ## TODO add test on raster data
+})
+
+test_that("qa_get can be passed either a dataset name as a string or a qa_dataset object", {
+    res1 <- qa_get("ADD Simple basemap")
+    res2 <- qa_get(qa_dataset("ADD Simple basemap"))
+    expect_identical(res1, res2)
 })
 
 test_that("qa_get can be passed a custom reader", {
