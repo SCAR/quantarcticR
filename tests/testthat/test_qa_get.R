71068f16c2493ad7504cd223d2a4a03773ee4405
Ben Raymond <ben.raymond@aad.gov.au>
2019-02-13 11:25:34 +1100

    test raster file type from qa_get

--- tests/testthat/test_qa_get.R
+++ tests/testthat/test_qa_get.R
@@ -3,7 +3,17 @@ context("Fetch Dataset")
 test_that("The dataset from qa_get returns a shapefile or raster, as appropriate", {
     res <- qa_get("ADD Simple basemap")
     expect_is(res, class = "SpatialPolygonsDataFrame")
-    ## TODO add test on raster data
+
+    res <- qa_get("ETOPO1/IBCSO/RAMP2 Hillshade (50x v. exag.)")
+    expect_is(res, class = "RasterLayer")
+
+    ## TODO also test other raster types:
+    ## "LIMA Landsat low-resolution mosaic (240m)" and "RAMP RADARSAT mosaic (100m)" are jp2
+    ## "USGS/NASA subantarctic Landsat (15m)" and "LIMA Landsat high-resolution virtual mosaic (15m)" are vrt (virtual rasters). For these, the vrt file points to other actual raster files
+
+    ## res <- qa_get("LIMA Landsat high-resolution virtual mosaic (15m)")
+    ## plot(crop(res, extent(c(200e4, 201e4, -1e4, 1e4))))
+    ## seems ok
 })
 
 test_that("qa_get can be passed either a dataset name as a string or a qa_dataset object", {
